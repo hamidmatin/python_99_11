5ea9844b5b4e302edca751416712badc042cc526
Hamid Reza Izadi Matin <hamid.matin@gmail.com>
2021-02-18 19:53:10 +0330

    Functions

---
+++ 6_built-in-Functions.py
@@ -0,0 +1,31 @@
+
+
+minimum = min(30,5,80,40,12,2,9,1,90)
+maximum = max(30,5,80,40,12,2,9,1,90)
+
+print(f'Minimum is {minimum}, Maximum is {maximum}')
+
+# message = input('Enter a Message : ')
+# print(f"Number of Characters {len(message)}")
+
+# import module_name 
+import math
+
+
+# module_name.function_name()
+
+x = 12.4999999
+print(f"round(x) = {round(x)}")
+print(f"math.ceil(x) = {math.ceil(x)}")
+print(f"math.floor(x) = {math.floor(x)}")
+print(f"math.trunc(x) = {math.trunc(x)}")
+
+#################
+import random
+print(random.random())
+print(random.randint(10, 50))
+
+############################################
+import arrow
+print(arrow.now())
+print(arrow.utcnow())
