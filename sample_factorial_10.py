a20811efb73c89b904065ede3625a9243c6eff4f
Hamid Reza Izadi Matin <hamid.matin@gmail.com>
2021-02-18 16:15:29 +0330

    Update

--- sample_factorial_10.py
+++ sample_factorial_10.py
@@ -8,4 +8,4 @@ else:
   if number > 0:
     for i in range(2, number + 1):
       factorial *= i
-  print(f"The factorial of number {number} is {factorial}")6
+  print(f"The factorial of number {number} is {factorial}")
