168f118994ee522109518204403c3d6934419252
Hamid Reza Izadi Matin <hamid.matin@gmail.com>
2021-03-04 16:07:17 +0330

    Start Session 5

--- 3_loop_continue.py
+++
@@ -1,8 +0,0 @@
-start = int(input('Enter a number for start : '))
-end = int(input('Enter a number for end : '))
-
-for i in range(start, end):
-  if(i % 3 == 0):
-    continue
-
-  print(f'Number is :{i}')
