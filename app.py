c7064b9db06f22c4954f66ebdaa6889d8c7fe45e
Hamid Reza Izadi Matin <hamid.matin@gmail.com>
2021-03-04 18:10:32 +0330

    Inner function and module

---
+++ app.py
@@ -0,0 +1,28 @@
+import inner_function_module
+
+my_global = 10
+
+def function_defination_with_no_argument():
+  print("This is a function definition with NO Argument")
+  # create local variable 
+  my_global = 60
+
+  print(f'My global at f1 is : {my_global}')
+
+def function_defination_with_one_argument(message):
+  print(f"This is a function definition with {message}")
+  # Use global keyword for change global variable
+  global my_global
+  my_global = 60
+
+def main():
+  print(f'My global at begining is : {my_global}')
+  function_defination_with_no_argument()
+  print(f'My global after call f1 is : {my_global}')
+  function_defination_with_one_argument("One Argument")
+  print(f'My global after call f2 is : {my_global}')
+
+  inner_function_module.outer_function()
+
+if(__name__ == "__main__"):
+  main()
