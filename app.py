6ef690e12d8fdce7097ad037fd3b9cffea3aa078
Hamid Reza Izadi Matin <hamid.matin@gmail.com>
2021-03-11 18:34:09 +0330

    List Intro and basic Operators

--- app.py
+++ app.py
@@ -1,48 +1,9 @@
-import inner_function_module
-import recursive_function
-import default_parameter
-
-
-def function_defination_with_no_argument():
-  print("This is a function definition with NO Argument")
-  # create local variable 
-  my_global = 60
-
-  print(f'My global at f1 is : {my_global}')
-
-def function_defination_with_one_argument(message):
-  print(f"This is a function definition with {message}")
-  # Use global keyword for change global variable
-  global my_global
-  my_global = 60
+import list
 
 def main():
-  print(f'My global at begining is : {my_global}')
-  function_defination_with_no_argument()
-  print(f'My global after call f1 is : {my_global}')
-  function_defination_with_one_argument("One Argument")
-  print(f'My global after call f2 is : {my_global}')
+  # list.list_intro()
+  # list.basic_list_operators()
+  list.ref_type_vs_value_type()
 
-  inner_function_module.outer_function()
-
-  # n = int(input('Enter a number : '))
-  n = 2
-  fac = recursive_function.factorial(n)
-  print(f'{n}! = {fac}')
-
-  fac = recursive_function.factorial_rec(n)
-  print(f'{n}! = {fac}')
-
-  #####################
-  default_parameter.work_area("Alice has interest in", "Internet of Things")
-  default_parameter.work_area("Sam works in")
-  default_parameter.work_area_2("Sam works in")
-  default_parameter.work_area_2("Sam works in", "LA")
-  default_parameter.work_area_2("Sam works in", city="LA")
-  default_parameter.work_area_2(prompt="Sam works in", domain="Internet of Things", city="LA")
-  default_parameter.work_area_2(city="LA", prompt="Sam works in", domain="Internet of Things")
-
-  print('1',end='')
-  print('2')
 if(__name__ == "__main__"):
   main()
