b4594e001c8ba498eab81dcf9e17a12d7e7d38d2
Hamid Reza Izadi Matin <hamid.matin@gmail.com>
2021-04-15 16:12:52 +0430

    Start Session 7

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-import list
 
 def main():
-  # list.list_intro()
-  # list.basic_list_operators()
-  # list.ref_type_vs_value_type()
-  # list.slicing()
-  # list.list_functions()
-  list.list_methods()
+  print('Session 7')
   
 if(__name__ == "__main__"):
   main()
