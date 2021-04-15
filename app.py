f079e23ac1744c01f4ce0d3e32db84a5cdff5da4
Hamid Reza Izadi Matin <hamid.matin@gmail.com>
2021-04-15 16:20:58 +0430

    Init

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 
 def main():
-  # list.list_intro()
-  # list.basic_list_operators()
-  # list.ref_type_vs_value_type()
-  # list.slicing()
-  # list.list_functions()
-  list.list_methods()
-
   # Start Session 07
   print('Session 7')
   
